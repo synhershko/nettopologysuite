720fe8882075195e7dd1dcda8ceac501cfd1d5d2
felix.obermaier@netcologne.de <felix.obermaier@netcologne.de@53ca42c3-9f19-0410-8ccb-b990a9bb5db6>
2010-09-10 08:22:47 +0000

    Branching to cleanup project

---
+++ branches/v2.11_cleanup/NetTopologySuite.Samples.Console/Operation/Linemerge/LineMergeExample.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using GeoAPI.Coordinates;
+using GeoAPI.DataStructures;
+using GeoAPI.Geometries;
+using GeoAPI.IO.WellKnownText;
+using GisSharpBlog.NetTopologySuite.Operation.Linemerge;
+using NetTopologySuite.Coordinates;
+using NUnit.Framework;
+
+namespace GisSharpBlog.NetTopologySuite.Samples.Operation.Linemerge
+{
+    /// <summary> 
+    /// Example of using the <see cref="LineMerger{TCoordinate}"/> class to 
+    /// sew together a set of fully noded linestrings.
+    /// </summary>	
+    [TestFixture]
+    public class LineMergeExample
+    {
+        private static IWktGeometryReader _reader;
+
+        static LineMergeExample()
+        {
+            InitBlock();
+        }
+
+        internal virtual IEnumerable<IGeometry<BufferedCoordinate>> Data
+        {
+            get
+            {
+                yield return Read("LINESTRING (220 160, 240 150, 270 150, 290 170)");
+                yield return Read("LINESTRING (60 210, 30 190, 30 160)");
+                yield return Read("LINESTRING (70 430, 100 430, 120 420, 140 400)");
+                yield return Read("LINESTRING (160 310, 160 280, 160 250, 170 230)");
+                yield return Read("LINESTRING (170 230, 180 210, 200 180, 220 160)");
+                yield return Read("LINESTRING (30 160, 40 150, 70 150)");
+                yield return Read("LINESTRING (160 310, 200 330, 220 340, 240 360)");
+                yield return Read("LINESTRING (140 400, 150 370, 160 340, 160 310)");
+                yield return Read("LINESTRING (160 310, 130 300, 100 290, 70 270)");
+                yield return Read("LINESTRING (240 360, 260 390, 260 410, 250 430)");
+                yield return Read("LINESTRING (70 150, 100 180, 100 200)");
+                yield return Read("LINESTRING (70 270, 60 260, 50 240, 50 220, 60 210)");
+                yield return Read("LINESTRING (100 200, 90 210, 60 210)");
+            }
+        }
+
+        private static void InitBlock()
+        {
+            _reader =
+                new WktReader<BufferedCoordinate>(
+                    GeometryServices.GetGeometryFactory(PrecisionModelType.DoubleFloating),
+                    null);
+        }
+
+        [STAThread]
+        public static void Main(String[] args)
+        {
+            LineMergeExample test = new LineMergeExample();
+
+            try
+            {
+                test.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        [Test]
+        public void Run()
+        {
+            IEnumerable<IGeometry<BufferedCoordinate>> lineStrings = Data;
+
+            LineMerger<BufferedCoordinate> lineMerger
+                = new LineMerger<BufferedCoordinate>();
+
+            lineMerger.Add(lineStrings);
+
+            IEnumerable<ILineString<BufferedCoordinate>> mergedLineStrings
+                = lineMerger.MergedLineStrings;
+
+            Console.WriteLine("Lines formed (" + Enumerable.Count(mergedLineStrings) + "):");
+
+            foreach (Object obj in mergedLineStrings)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+
+
+        internal virtual IGeometry<BufferedCoordinate> Read(String lineWKT)
+        {
+            try
+            {
+                IGeometry<BufferedCoordinate> geom
+                    = _reader.Read(lineWKT) as IGeometry<BufferedCoordinate>;
+                return geom;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ branches/v2.11_cleanup/NetTopologySuite/Geometries/Utilities/GeometryFilter.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using GeoAPI.Coordinates;
+using GeoAPI.Geometries;
+using NPack.Interfaces;
+
+namespace GisSharpBlog.NetTopologySuite.Geometries.Utilities
+{
+    internal static class GeometryFilter
+    {
+
+        public static IEnumerable<TGeometry> Extract<TGeometry, TCoordinate>(IGeometry<TCoordinate> geometry)
+            where TGeometry : IGeometry<TCoordinate>
+            where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>, IComparable<TCoordinate>,
+                IComputable<Double, TCoordinate>, IConvertible
+        {
+            if (geometry is TGeometry)
+            {
+                yield return (TGeometry)geometry;
+            }
+
+            /*if (geometry is IHasGeometryComponents<TCoordinate>)
+            {
+            }*/
+
+            if (geometry is IGeometryCollection<TCoordinate>)
+            {
+                foreach (IGeometry<TCoordinate> g in (IGeometryCollection<TCoordinate>)geometry)
+                {
+                    foreach (TGeometry g2 in Extract<TGeometry, TCoordinate>(g))
+                        yield return g2;
+                    
+                }
+            }
+        }
+
+        public static IEnumerable<TGeometry> Filter<TGeometry, TCoordinate>(IGeometry<TCoordinate> geometry)
+            where TGeometry : IGeometry<TCoordinate>
+            where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>, IComparable<TCoordinate>,
+                IComputable<Double, TCoordinate>, IConvertible
+        {
+            if (geometry is TGeometry)
+            {
+                yield return (TGeometry)geometry;
+            }
+
+            if (geometry is IHasGeometryComponents<TCoordinate>)
+            {
+                foreach (IGeometry<TCoordinate> g in (((IHasGeometryComponents<TCoordinate>)geometry).Components))
+                {
+                    foreach( TGeometry g2 in Filter<TGeometry, TCoordinate>(g))
+                        yield return g2;
+                }
+                yield break;
+            }
+
+            if (geometry is IGeometryCollection<TCoordinate>)
+            {
+                foreach (TGeometry g in Filter<TGeometry, TCoordinate>((IGeometryCollection<TCoordinate>)geometry))
+                {
+                    yield return g;
+                }
+            }
+
+            //if (geometry is IEnumerable<IGeometry>)
+            //{
+            //    foreach (TGeometry g in Filter<TGeometry>(geometry as IEnumerable<IGeometry>))
+            //    {
+            //        yield return g;
+            //    }
+            //}
+        }
+
+        public static IEnumerable<TGeometry> Filter<TGeometry, TCoordinate>(
+            IGeometryCollection<TCoordinate> geometries)
+            where TGeometry : IGeometry<TCoordinate>
+            where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>, IComparable<TCoordinate>,
+                IComputable<Double, TCoordinate>, IConvertible
+        {
+            foreach (IGeometry<TCoordinate> geometry in geometries)
+            {
+                if (geometry is TGeometry)
+                {
+                    yield return (TGeometry)geometry;
+                }
+            }
+        }
+
+        //public static IEnumerable<TGeometry> Filter<TGeometry, TCoordinate>(IGeometry<TCoordinate> geometry)
+        //    where TGeometry : IGeometry<TCoordinate>
+        //    where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>, IComparable<TCoordinate>,
+        //        IComputable<Double, TCoordinate>, IConvertible
+
+        //{
+        //    if (geometry is TGeometry)
+        //    {
+        //        yield return (TGeometry) geometry;
+        //    }
+
+        //    if (geometry is IHasGeometryComponents<TCoordinate>)
+        //    {
+        //        foreach (TGeometry g in Filter<TGeometry, TCoordinate>((IHasGeometryComponents<TCoordinate>) geometry))
+        //        {
+        //            yield return g;
+        //        }
+        //    }
+
+        //    //if (geometry is IEnumerable<IGeometry>)
+        //    //{
+        //    //    foreach (TGeometry g in Filter<TGeometry>(geometry as IEnumerable<IGeometry>))
+        //    //    {
+        //    //        yield return g;
+        //    //    }
+        //    //}
+        //}
+
+        //public static IEnumerable<TGeometry> Filter<TGeometry, TCoordinate>(
+        //    IHasGeometryComponents<TCoordinate> geometries)
+        //    where TGeometry : IGeometry<TCoordinate>
+        //    where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>, IComparable<TCoordinate>,
+        //        IComputable<Double, TCoordinate>, IConvertible
+        //{
+        //    foreach (IGeometry<TCoordinate> geometry in geometries.Components)
+        //    {
+        //        if (geometry is TGeometry)
+        //        {
+        //            yield return (TGeometry) geometry;
+        //        }
+        //    }
+        //}
+
+        public static void Apply<TGeometry>(IEnumerable<TGeometry> geometries, Action<TGeometry> action)
+            where TGeometry : IGeometry
+        {
+            foreach (TGeometry geometry in geometries)
+            {
+                action(geometry);
+            }
+        }
+
+        public static IEnumerable<TOutput> Apply<TGeometry, TOutput>(IEnumerable<TGeometry> geometries,
+                                                                     Func<TGeometry, TOutput> function)
+            where TGeometry : IGeometry
+        {
+            foreach (TGeometry geometry in geometries)
+            {
+                yield return function(geometry);
+            }
+        }
+    }
+
+    internal static class CoordinateSequenceFilter
+    {
+        public static IEnumerable<ICoordinateSequence<TCoordinate>> Filter<TCoordinate>(IGeometry<TCoordinate> geometry)
+            where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>, IComparable<TCoordinate>,
+                IComputable<Double, TCoordinate>, IConvertible
+        {
+            if (geometry is IHasGeometryComponents<TCoordinate>)
+            {
+                foreach (IGeometry<TCoordinate> g in ((IHasGeometryComponents<TCoordinate>) geometry).Components)
+                {
+                    if (g is ICurve<TCoordinate>)
+                        yield return geometry.Coordinates;
+                }
+            }
+
+            if (geometry is ICurve<TCoordinate>)
+            {
+                yield return geometry.Coordinates;
+            }
+        }
+
+        public static IEnumerable<ICoordinateSequence<TCoordinate>> Filter<TCoordinate>(
+            IHasGeometryComponents<TCoordinate> geometries)
+            where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>, IComparable<TCoordinate>,
+                IComputable<Double, TCoordinate>, IConvertible
+        {
+            foreach (IGeometry<TCoordinate> geometry in geometries.Components)
+            {
+                if (geometry is IHasGeometryComponents<TCoordinate>)
+                {
+                    foreach (IGeometry<TCoordinate> g in Filter((IHasGeometryComponents<TCoordinate>) geometry))
+                    {
+                        if (g is ICurve<TCoordinate>)
+                            yield return geometry.Coordinates;
+                    }
+                }
+
+                if (geometry is ICurve<TCoordinate>)
+                    yield return geometry.Coordinates;
+            }
+        }
+
+        public static void Apply<TCoordinate>(IEnumerable<ICoordinateSequence<TCoordinate>> sequences,
+                                              Action<ICoordinateSequence<TCoordinate>> action)
+            where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>, IComparable<TCoordinate>,
+                IComputable<Double, TCoordinate>, IConvertible
+        {
+            foreach (ICoordinateSequence<TCoordinate> sequence in sequences)
+            {
+                action(sequence);
+            }
+        }
+
+        public static IEnumerable<TOutput> Apply<TCoordinate, TOutput>(
+            IEnumerable<ICoordinateSequence<TCoordinate>> sequences,
+            Func<ICoordinateSequence<TCoordinate>, TOutput> function)
+            where TCoordinate : ICoordinate<TCoordinate>, IEquatable<TCoordinate>, IComparable<TCoordinate>,
+                IComputable<Double, TCoordinate>, IConvertible
+        {
+            foreach (ICoordinateSequence<TCoordinate> sequence in sequences)
+            {
+                yield return function(sequence);
+            }
+        }
+    }
+}
