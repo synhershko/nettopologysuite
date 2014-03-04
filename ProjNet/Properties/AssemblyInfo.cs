2d487c5bf4452410397112d020560a72e0fb72ba
diegoguidi <diegoguidi@53ca42c3-9f19-0410-8ccb-b990a9bb5db6>
2014-03-04 14:44:49 +0000

    tagging ProjNet v.1.3.0.3 (used by NTS 1.13.2)

---
+++ ProjNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Proj.NET")]
+[assembly: AssemblyDescription("Proj.NET performs point-to-point coordinate conversions between geodetic coordinate systems for use in .Net, Geographic Information Systems (GIS) or GPS applications.\nThe spatial reference model used adheres to the Simple Features specification.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Stable")]
+#endif
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Proj.NET")]
+[assembly: AssemblyCopyright("Copyright © 2009 - 2013 Morten Nielsen, NetTopologySuite - Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: System.CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("e16f3ac5-214b-4ef3-9809-740a13cf0ec7")]
+[assembly: AssemblyVersion("1.3.0.3")]
+[assembly: AssemblyFileVersion("1.3.0.3")]
