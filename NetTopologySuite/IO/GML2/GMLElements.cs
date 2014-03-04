dfb110557c197f3df5532a5d0b9f2f3b73c24ecc
diegoguidi <diegoguidi@53ca42c3-9f19-0410-8ccb-b990a9bb5db6>
2014-03-04 14:46:41 +0000

    tagging PowerCollections code used by NTS 1.13.2

--- NetTopologySuite/IO/GML2/GMLElements.cs
+++
@@ -1,9 +0,0 @@
-namespace NetTopologySuite.IO.GML2
-{
-    internal static class GMLElements
-    {
-        internal const string gmlPrefix = "gml";
-        internal const string gmlNS = "http://www.opengis.net/gml";
-        internal const string gmlNSDecl = " xmlns:gml=\"" + gmlNS + "\"";
-    }
-}
